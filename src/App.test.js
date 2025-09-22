2ff36ef881cce5047d6345168d1714283628fd0c
kamalesh <kamalikamalesh0401@gmail.com>
2025-09-22 18:33:51 +0530

    22-sep-2025 Bug fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,3 @@
-// import { render, screen } from "@testing-library/react";
-// import App from "./App";
-
-// test("renders app header", () => {
-//   render(<App />);
-//   const headerElement = screen.getByText(/Developer Tools Hub/i);
-//   expect(headerElement).toBeInTheDocument();
-// });
-
-// import { render, screen } from "@testing-library/react";
-// import App from "./App";
-
-// test("renders tab buttons", () => {
-//   render(<App />);
-//   expect(screen.getByText(/JSON/i)).toBeInTheDocument();
-//   expect(screen.getByText(/JWT/i)).toBeInTheDocument();
-//   expect(screen.getByText(/Base64/i)).toBeInTheDocument();
-// });
-
 import { render } from "@testing-library/react";
 import App from "./App";
 
